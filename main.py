76b40698c80d22144ad65d817fe32b7e8f902028
Z3rnix <dantesoulreaper18@gmail.com>
2026-08-30 19:45:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ async def hablar(datos: MensajeInput):
 
         # Llamada al modelo Gemini 2.5 Flash
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.6-flash",
             contents=datos.texto,
         )
 
