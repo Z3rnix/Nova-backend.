c72945bedc8eeaa77e0865187b16a195eb5da6cb
Z3rnix <dantesoulreaper18@gmail.com>
2026-08-30 20:45:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ async def hablar_con_nova(request: QueryRequest):
                 {"role": "system", "content": SYSTEM_PROMPT},
                 {"role": "user", "content": request.texto}
             ],
-            model="llama-3.1 -8b- instant",
+            model="llama3-8b-8192",
             temperature=0.7,
         )
 
